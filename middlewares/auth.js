9439af544f3a822f1c63e6fb01c143cbe24e1999
naor136 <110901836+naor136@users.noreply.github.com>
2023-02-06 13:16:45 +0200

    Add files via upload

---
+++ middlewares/auth.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+
+exports.auth = (req,res,next) => {
+  let token = req.header("x-api-key");
+  if(!token){
+    return res.status(401).json({msg:"You must send token in the header to this endpoint"})
+  }
+  try{
+    // בודק אם הטוקן תקין או בתקוף
+    let decodeToken = jwt.verify(token,"ditoSecret");
+    // req -> יהיה זהה בכל הפונקציות שמורשרות באותו ראוטר
+    req.tokenData = decodeToken;
+    // לעבור לפונקציה הבאה בשרשור
+    next();
+  }
+  catch(err){
+    return res.status(401).json({msg:"Token invalid or expired"})
+  }
+}
+
+// auth for admin only
+exports.authAdmin = (req,res,next) => {
+  let token = req.header("x-api-key");
+  if(!token){
+    return res.status(401).json({msg:"You must send token in the header to this endpoint"})
+  }
+  try{
+    // בודק אם הטוקן תקין או בתקוף
+    let decodeToken = jwt.verify(token,"ditoSecret");
+    // בודק אם הטוקן שייך לאדמין
+    if(decodeToken.role != "admin"){
+      return res.status(401).json({msg:"Just admin can be in this endpoint"})
+    }
+    // req -> יהיה זהה בכל הפונקציות שמורשרות באותו ראוטר
+    req.tokenData = decodeToken;
+    // לעבור לפונקציה הבאה בשרשור
+    next();
+  }
+  catch(err){
+    return res.status(401).json({msg:"Token invalid or expired"})
+  }
+}
